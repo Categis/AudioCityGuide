b124d55c2c61bb1a7ff1cf88d9b55ae0cad41f87
maheshkandhari <mahesh.kandhari@gmail.com>
2017-03-02 17:20:28 +0530

    Modified code to get full size camera photo on capture

--- app/src/main/java/solutiontogo/de/audiocitytourguide/CameraDemoActivity.java
+++ app/src/main/java/solutiontogo/de/audiocitytourguide/CameraDemoActivity.java
@@ -1,20 +1,25 @@
 package solutiontogo.de.audiocitytourguide;
 
 import android.app.Activity;
+import android.content.ContentResolver;
 import android.content.Intent;
+import android.graphics.Bitmap;
 import android.net.Uri;
 import android.os.Bundle;
 import android.os.Environment;
 import android.provider.MediaStore;
-import android.util.Log;
-import android.view.View;
-import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.Toast;
 
-import java.io.File;
-import java.io.IOException;
+import com.amazonaws.mobileconnectors.s3.transferutility.TransferObserver;
+import com.amazonaws.mobileconnectors.s3.transferutility.TransferType;
+import com.amazonaws.mobileconnectors.s3.transferutility.TransferUtility;
 
-import static android.app.Activity.RESULT_OK;
+import java.io.File;
+import java.util.List;
+
+import solutiontogo.de.audiocitytourguide.utils.AmazonS3Constants;
+import solutiontogo.de.audiocitytourguide.utils.AmazonS3Utility;
 
 /**
  * Created by shivaramak on 30/12/2016.
@@ -23,6 +28,7 @@ import static android.app.Activity.RESULT_OK;
 public class CameraDemoActivity extends Activity {
     int TAKE_PHOTO_CODE = 0;
     public static int count = 0;
+    private Uri mImageUri;
 
     /** Called when the activity is first created. */
     @Override
@@ -31,7 +37,30 @@ public class CameraDemoActivity extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.take_picture);
 
-        // Here, we are making a folder named picFolder to store
+
+
+        Intent intent = new Intent("android.media.action.IMAGE_CAPTURE");
+        File photo = null;
+        try
+        {
+            // place where to store camera taken picture
+            photo = this.createTemporaryFile("picture", ".jpg");
+            photo.delete();
+        }
+        catch(Exception e)
+        {
+
+            Toast.makeText(getApplicationContext(), "Please check SD card! Image shot is impossible!", Toast.LENGTH_SHORT).show();
+
+        }
+        mImageUri = Uri.fromFile(photo);
+        intent.putExtra(MediaStore.EXTRA_OUTPUT, mImageUri);
+        //start camera intent
+        startActivityForResult(intent, 0);
+
+
+
+        /*// Here, we are making a folder named picFolder to store
         // pics taken by the camera using this application.
         final String dir = Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_PICTURES) + "/picFolder/";
         File newdir = new File(dir);
@@ -61,10 +90,19 @@ public class CameraDemoActivity extends Activity {
 
                 startActivityForResult(cameraIntent, TAKE_PHOTO_CODE);
             }
-        });
+        });*/
     }
-
-    @Override
+    private File createTemporaryFile(String part, String ext) throws Exception
+    {
+        File tempDir= Environment.getExternalStorageDirectory();
+        tempDir=new File(tempDir.getAbsolutePath()+"/.temp/");
+        if(!tempDir.exists())
+        {
+            tempDir.mkdirs();
+        }
+        return File.createTempFile(part, ext, tempDir);
+    }
+/*    @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
 
@@ -72,5 +110,42 @@ public class CameraDemoActivity extends Activity {
             Toast.makeText(getApplicationContext(), "Camera Pic Saved", Toast.LENGTH_SHORT).show();
             Log.d("CameraDemo", "Pic saved");
         }
+    }*/
+
+    public void grabImage(ImageView imageView)
+    {
+        this.getContentResolver().notifyChange(mImageUri, null);
+        ContentResolver cr = this.getContentResolver();
+        Bitmap bitmap;
+        try
+        {
+            TransferUtility transferUtility = AmazonS3Utility.getTransferUtility(this);
+            List<TransferObserver> observers;
+            observers = transferUtility.getTransfersWithType(TransferType.UPLOAD);
+            bitmap = android.provider.MediaStore.Images.Media.getBitmap(cr, mImageUri);
+            imageView.setImageBitmap(bitmap);
+            File file = new File(mImageUri.getPath());
+            TransferObserver observer = transferUtility.upload(AmazonS3Constants.BUCKET_NAME, file.getName(),
+                    file);
+        }
+        catch (Exception e)
+        {
+            Toast.makeText(this, "Failed to load", Toast.LENGTH_SHORT).show();
+
+        }
+    }
+
+
+    //called after camera intent finished
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent intent)
+    {
+        if(requestCode==0 && resultCode==RESULT_OK)
+        {
+            ImageView imageView1 = (ImageView) findViewById(R.id.imageView1);
+            //... some code to inflate/create/find appropriate ImageView to place grabbed image
+            this.grabImage(imageView1);
+        }
+        super.onActivityResult(requestCode, resultCode, intent);
     }
 }
